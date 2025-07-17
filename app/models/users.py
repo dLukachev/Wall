c4fa1962a081e641ed1ac412131173b7fba9c678
dLukachev <a89266384045@gmail.com>
2025-07-18 01:16:55 +0300

    Подключил алембик и добавил поле пароль у юзера

--- app/models/users.py
+++ app/models/users.py
@@ -10,6 +10,7 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     nickname = Column(String(50), unique=True, nullable=False)
     name = Column(String(50), nullable=False)
+    password = Column(String(50), nullable=False)
     created_at = Column(DateTime, default=datetime.datetime.now(datetime.UTC))
 
     posts = relationship('Post', back_populates='user', cascade='all, delete-orphan')
